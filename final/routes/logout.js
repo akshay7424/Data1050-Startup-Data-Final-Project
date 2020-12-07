7f186f0b807f0408a2b0bb68a0933a26edec61a8
Tasha Kim <jae_young_kim@brown.edu>
2020-12-07 23:56:55 +0900

    Prediction map updated, toggle buttons, iframe fixed

--- final/routes/logout.js
+++ final/routes/logout.js
@@ -1,19 +1,17 @@
 var express = require("express");
 var router = express.Router();
 
+router.get("/", function (req, res) {
+  // Manual reset
+  req.session.uid = null;
+  req.session.name = null;
+  req.session.email = null;
+  req.session.year = null;
+  req.session.password = null;
 
-router.get('/', function (req, res) {
-    // Manual reset
-    req.session.uid = null;
-    req.session.name = null;
-    req.session.email = null;
-    req.session.year = null;
-    req.session.password = null;
-
-    console.log("User successfully logged out.")
-    res.redirect('/');
-    return;
+  console.log("User successfully logged out.");
+  res.redirect("/login");
+  return;
 });
-
 
 module.exports = router;
